0e57a53eb1da02496eaf358e474ae0fe2d862291
Muhamad Fauzan Al Azhar <fauzanalazhar9@gmail.com>
2025-12-29 17:34:36 +0700

    nambah migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,14 +9,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('google_id')->unique()->nullable(); // HAPUS ->after('id')
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('avatar')->nullable(); // HAPUS ->after('email')
+            $table->bigIncrements('id')->unsigned();
+            $table->string('google_id', 255)->unique()->nullable();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
+            $table->string('phone', 255)->nullable();
+            $table->text('address')->nullable();
+            $table->string('city', 255)->nullable();
+            $table->string('province', 255)->nullable();
+            $table->string('postal_code', 255)->nullable();
+            $table->string('avatar', 255)->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->rememberToken();
+            $table->string('password', 255)->nullable();
+            $table->string('role', 255)->default('customer');
+            $table->string('remember_token', 100)->nullable();
             $table->timestamps();
         });
     }
