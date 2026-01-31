540ea53634665f7758187bda401e02e8c1e6e2a6
rakslii <rasya234sud@gmail.com>
2026-01-31 15:39:23 +0700

    storage

---
+++ storage/framework/views/f3a5efbfc1be2331502d6f85577ae84b.php
@@ -0,0 +1,63 @@
+<?php $attributes ??= new \Illuminate\View\ComponentAttributeBag;
+
+$__newAttributes = [];
+$__propNames = \Illuminate\View\ComponentAttributeBag::extractPropNames((['id', 'title']));
+
+foreach ($attributes->all() as $__key => $__value) {
+    if (in_array($__key, $__propNames)) {
+        $$__key = $$__key ?? $__value;
+    } else {
+        $__newAttributes[$__key] = $__value;
+    }
+}
+
+$attributes = new \Illuminate\View\ComponentAttributeBag($__newAttributes);
+
+unset($__propNames);
+unset($__newAttributes);
+
+foreach (array_filter((['id', 'title']), 'is_string', ARRAY_FILTER_USE_KEY) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+}
+
+$__defined_vars = get_defined_vars();
+
+foreach ($attributes->all() as $__key => $__value) {
+    if (array_key_exists($__key, $__defined_vars)) unset($$__key);
+}
+
+unset($__defined_vars, $__key, $__value); ?>
+
+<div id="<?php echo e($id); ?>" class="fixed inset-0 bg-black bg-opacity-50 hidden z-50">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="bg-white rounded-lg shadow-xl w-full max-w-lg mx-4">
+            <!-- Header -->
+            <div class="flex justify-between items-center p-6 border-b">
+                <h3 class="text-lg font-semibold"><?php echo e($title); ?></h3>
+                <button onclick="document.getElementById('<?php echo e($id); ?>').classList.add('hidden')" 
+                        class="text-gray-500 hover:text-gray-700">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+            
+            <!-- Content -->
+            <div class="p-6">
+                <?php echo e($slot ?? ''); ?> <!-- ⚡ TAMBAH null coalescing -->
+            </div>
+            
+            <!-- Footer -->
+            <?php if(!empty($slot)): ?>
+            <div class="p-6 border-t flex justify-end space-x-3">
+                <button onclick="document.getElementById('<?php echo e($id); ?>').classList.add('hidden')"
+                        class="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50">
+                    Cancel
+                </button>
+                <button type="submit" form="<?php echo e($id); ?>-form"
+                        class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                    Save
+                </button>
+            </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div><?php /**PATH C:\laragon\www\revisiimaji\resources\views/pages/admin/components/modal.blade.php ENDPATH**/ ?>
